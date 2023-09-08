d85018409237a874b192944536e6112ef19ff0ba
Avinash-jetwani <jetwaniavinash@gmail.com>
2023-09-08 23:50:50 +0100

    Connect to MongoDB and add sample API endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// To set up basic Express server
 const express = require('express');
 const app = express();
 const port = 3001;  // You can choose any port number
@@ -10,3 +11,26 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
+// To connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://localhost:27017/myMovieDB";  // Replace with your MongoDB URI
+
+MongoClient.connect(uri, function(err, client) {
+    if(err) {
+      console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+    }
+    console.log('Connected...');
+    client.close();
+  });
+  
+
+// API endpoint
+app.get('/movies', (req, res) => {
+    // For now, let's return a static array
+    res.json([
+      { title: 'Inception', year: 2010 },
+      { title: 'Interstellar', year: 2014 }
+    ]);
+  });
+  
+  
