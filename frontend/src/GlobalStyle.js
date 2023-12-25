db7d638f4c486b4c532a40aa6896795465fb15e4
Avinash-jetwani <jetwaniavinash@gmail.com>
2023-12-25 22:48:30 +0000

    Changes for mobile and tablet responsive.

--- frontend/src/GlobalStyle.js
+++ frontend/src/GlobalStyle.js
@@ -4,9 +4,26 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    padding: 0;
     font-family: 'Roboto', sans-serif;
     background-color: black;
     color: white;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  // Example media query for tablets
+  @media (max-width: 768px) {
+    body {
+      background-color: darkgray;
+    }
+  }
+
+  // Example media query for mobile devices
+  @media (max-width: 480px) {
+    body {
+      background-color: lightgray;
+    }
   }
 `;
 
